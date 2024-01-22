13255307579786ae7663d613df9cd7b92d7f0eb6
Selim Şahin <selimsahin.sns@gmail.com>
2024-01-22 03:31:30 +0300

    Created AppDbContext, Platform model and PlatformRepository.

---
+++ PlatformService/Data/IPlatformRepository.cs
@@ -0,0 +1,15 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepository
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+
+        Platform GetPlatformById(int id);
+
+        void CreatePlatform(Platform platform);
+    }
+}
