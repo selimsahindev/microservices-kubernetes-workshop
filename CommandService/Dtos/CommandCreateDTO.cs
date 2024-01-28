b03590c17a52f6821fb0fdefb05fd27e69b25b13
Selim Şahin <selimsahin.sns@gmail.com>
2024-01-28 13:27:58 +0300

    added AppDbContext, command repository class and required DTOs for the command service

---
+++ CommandService/Dtos/CommandCreateDTO.cs
@@ -0,0 +1,7 @@
+namespace CommandService.Dtos
+{
+    public class CommandCreateDTO
+    {
+
+    }
+}
---
+++ CommandService/Dtos/PlatformReadDTO.cs
@@ -0,0 +1,9 @@
+namespace CommandService.Dtos
+{
+    public class PlatformReadDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
